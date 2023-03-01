383879fb7bc07dd1628a701cfb0a7b029812e87c
beinganidiot <ocaranzacosta@gmail.com>
2023-03-01 14:03:53 +0000

    final result: includes automatic timer + reload button

--- src/app.js
+++ src/app.js
@@ -2,10 +2,24 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector(".card").classList.add(randomSuit());
+  document.querySelector(".card").innerHTML = randomNumber();
+  setInterval(() => {
+    document.querySelector(".card").classList.remove(randomSuit());
+    document.querySelector(".card").classList.add(randomSuit());
+    document.querySelector(".card").innerHTML = randomNumber();
+  }, 10000);
 };
+
+function randomNumber() {
+  let nmb = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+  let picked = Math.floor(Math.random() * nmb.length);
+  return nmb[picked];
+}
+
+function randomSuit() {
+  let suit = ["diamond", "spades", "heart", "club"];
+  let picked = Math.floor(Math.random() * suit.length);
+  return suit[picked];
+}
